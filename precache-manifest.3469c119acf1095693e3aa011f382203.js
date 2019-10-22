1b9552fe53c35c6a00d05e1b2519be2b4a463a4f
AninditaDe <aninditade2@gmail.com>
2019-10-22 19:22:22 +0100

    Updates

---
+++ precache-manifest.3469c119acf1095693e3aa011f382203.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78511900a9ee2af9a7d1f0e21ee6bca4",
+    "url": "/AninditaDe/PeLableApp.git/index.html"
+  },
+  {
+    "revision": "c6d269bcf07ad922db6d",
+    "url": "/AninditaDe/PeLableApp.git/static/css/2.5d45f07d.chunk.css"
+  },
+  {
+    "revision": "4a539fdb4a837c93f72c",
+    "url": "/AninditaDe/PeLableApp.git/static/css/main.63818607.chunk.css"
+  },
+  {
+    "revision": "c6d269bcf07ad922db6d",
+    "url": "/AninditaDe/PeLableApp.git/static/js/2.f15758db.chunk.js"
+  },
+  {
+    "revision": "4a539fdb4a837c93f72c",
+    "url": "/AninditaDe/PeLableApp.git/static/js/main.7467a418.chunk.js"
+  },
+  {
+    "revision": "e48e9157b0d881299564",
+    "url": "/AninditaDe/PeLableApp.git/static/js/runtime-main.dae9df49.js"
+  },
+  {
+    "revision": "f70991e680a7d72b50fbacebbc7ce5e4",
+    "url": "/AninditaDe/PeLableApp.git/static/media/Logo.f70991e6.jpg"
+  }
+]);
